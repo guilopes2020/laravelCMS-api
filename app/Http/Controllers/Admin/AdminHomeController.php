e7a6d6ce1f03535b774f0e37be9b70c651419f56
guilopes2020 <guilopesdev@gmail.com>
2024-08-06 14:21:52 -0300

    feat: :sparkles: dashboard-configurations

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Page;
+use App\Models\User;
+use App\Models\Visitor;
 
 class AdminHomeController extends Controller
 {
@@ -12,8 +15,45 @@ class AdminHomeController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.home');
+        $visitsCount = 0;
+        $onlineCount = 0;
+        $pageCount = 0;
+        $userCount = 0;
+        $interval = intval($request->input('interval', 30));
+        if ($interval > 120) {
+            $interval = 120;
+        }
+
+        $dateInterval = date('Y-m-d H:i:s', strtotime('-'.$interval.' days'));
+        $visitsCount = Visitor::where('date_access', '>=', $dateInterval)->count();
+
+        $dateLimit = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+        $onlineList = Visitor::select('ip')->where('date_access', '>=', $dateLimit)->groupBy('ip')->get();
+        $onlineCount = count($onlineList);
+
+        $pageCount = Page::count();
+
+        $userCount = User::count();
+
+        $pagePie = [];
+        $visitsAll = Visitor::selectRaw('page, count(page) as c')->where('date_access', '>=', $dateInterval)->groupBy('page')->get();
+        foreach($visitsAll as $visit) {
+            $pagePie[$visit['page']] = intval($visit['c']);
+        }
+
+        $pageLabels = json_encode(array_keys($pagePie));
+        $pageValues = json_encode(array_values($pagePie));
+
+        return view('admin.home', [
+            'visitsCount'  => $visitsCount,
+            'onlineCount'  => $onlineCount,
+            'pageCount'    => $pageCount,
+            'userCount'    => $userCount,
+            'pageLabels'   => $pageLabels,
+            'pageValues'   => $pageValues,
+            'dateInterval' => $interval
+        ]);
     }
 }
